2ecde6c1eb783bafb1d4c01f884f83a60aa388c7
Noah Olsson <noaholsson@hotmail.com>
2018-08-27 20:04:48 +0200

    Fixes

--- api/index.php
+++ api/index.php
@@ -16,5 +16,20 @@ if ($result->num_rows > 0) {
     }
 }
 
+$sql = "SELECT * FROM humidity";
+$result = $conn->query($sql);
+
+$humidity = array();
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $humidity[] = array(
+            'value' => $row['value'],
+            'date' => $row['date']
+        );
+    }
+}
+
 header("Content-Type: application/json");
-echo "'" . json_encode(array('data' => $temperatures)) . "'";
+echo "'" . json_encode(array('temperatures' => $temperatures, 'humidity' => $humidity)) . "'";
