69988cad61606f7d1da49ceb67575ddd7902958f
Noah Olsson <noaholsson@hotmail.com>
2018-11-03 16:32:42 +0100

    Fixes

--- api/index.php
+++ api/index.php
@@ -1,21 +1,28 @@
 <?php 
 require_once "../includes/database.php";
 
+header("Content-Type: application/json");
 
 
-if( !isset($_POST['action']) ){
-    header("Content-Type: application/json");
-        echo "'" . json_encode( array('error' => "No action specified") ) . "'";
+if( !isset($_GET['action']) ){
+    http_response_code (400);
+    echo "'" . json_encode( array('error' => "No action specified") ) . "'";
     return;
 }
-$action = $_POST['action'];
+$action = $_GET['action'];
 
 switch($action){
     case "get_between":
 
+        if(!isset($_GET['first_date']) && !isset($_GET['last_date'])) {
+            http_response_code (400);
+            echo "'" . json_encode( array('error' => "You need to specify dates") ) . "'";
+            return;
+        }
+
         $date = date("Y-m-d");
 
-        $sql = "SELECT date, AVG(value) AS value FROM temperature WHERE date BETWEEN '2018-08-29' AND '2018-08-30' GROUP BY date";
+        $sql = "SELECT date, AVG(value) AS value FROM temperature WHERE date BETWEEN '$_GET[first_date]' AND '$_GET[last_date]' GROUP BY date";
         $result = $conn->query($sql);
 
         $temperatures = array();
@@ -30,7 +37,7 @@ switch($action){
             }
         }
 
-        $sql = "SELECT date, AVG(value) AS value FROM humidity WHERE date BETWEEN '2018-08-29' AND '2018-08-30' GROUP BY date";
+        $sql = "SELECT date, AVG(value) AS value FROM humidity WHERE date BETWEEN '$_GET[first_date]' AND '$_GET[last_date]' GROUP BY date";
         $result = $conn->query($sql);
 
         $humidity = array();
@@ -44,15 +51,52 @@ switch($action){
                 );
             }
         }
-
-        header("Content-Type: application/json");
+        http_response_code (200);
         echo "'" . json_encode(array('temperatures' => $temperatures, 'humidity' => $humidity)) . "'";
 
         return;
 
     case "get_weeks":
-        
-        
+
+        $weeks = array();
+
+        $sql = "SELECT date FROM temperature GROUP BY date";
+        $result = $conn->query($sql);
+
+        $humidity = array();
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                $date = new DateTime($row['date']);
+                $title = $date->format("Y V. W");
+
+                $weeks[$title][] = $row['date'];
+            }
+        }
+
+        foreach($weeks as $title => $week){
+            $first_date = $week[0];
+            $last_date = $week[0];
+
+            foreach($week as $date){
+                if(strtotime($date) < strtotime($first_date)){
+                    $first_date = $date;
+                }
+                if(strtotime($date) > strtotime($last_date)){
+                    $last_date = $date;
+                }
+            }
+
+            $weeks[$title] = array(
+                'firstDate' => $first_date,
+                'lastDate' => $last_date,
+            );
+
+        }
+        http_response_code (200);
+        echo "'" . json_encode(array('weeks' => $weeks)) . "'";
+        return;
 
 }
 
